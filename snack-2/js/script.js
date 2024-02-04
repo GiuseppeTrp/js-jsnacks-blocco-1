685c9b0ae563c53cbc14b0d33e12a0d718e381ea
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-02-04 21:01:39 +0100

    ended exs. 2

---
+++ snack-2/js/script.js
@@ -0,0 +1,30 @@
+// creo una funzione per poter ripetere il ciclo ad ogni click dell'utente sul button
+function repeat() {
+// Variabile per la somma 
+let sum = 0;
+
+// Ciclo 'for'fino a quando il prompt non viene ripetuto per 10 volte
+for (let i = 0; i < 10; i++) {
+    // Prompt per l'inserimento dati
+    let number = (prompt("Inserisci un valore"));
+
+    // conversione della stringa in numeri e somma finale 
+    sum = sum + number;
+}
+
+// inserisco una condizione 
+if (isNaN(sum)) {
+        // stampo il risultato 
+
+    // se sum non è un numero allora appare un alert con il seguente messaggio
+    alert("HAI INSERITO CARATTERI NON CONSENTITI. INSERIRE UNICAMENTE NUMERI.");
+} else {
+    // stampo il risultato 
+    document.getElementById("result").innerHTML = sum;
+}
+}
+// evento listener per il pulsante
+document.getElementById("repeat").addEventListener("click", repeat);
+
+// ripete la funzione al click
+repeat();
